c7d7298fe1846b4ecd10f082b3101acd7fcae0d1
kevinVtt <kevinvittor579@gmail.com>
2025-09-23 11:53:07 -0300

    deploy

--- src/test/java/com/acelerador/polo_it_acelerador/PoloItAceleradorApplicationTests.java
+++ src/test/java/com/acelerador/polo_it_acelerador/PoloItAceleradorApplicationTests.java
@@ -2,8 +2,10 @@ package com.acelerador.polo_it_acelerador;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PoloItAceleradorApplicationTests {
 
 	@Test
