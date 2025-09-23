ed7dbed69406a559893252664d562b10b77de5d6
kevinVtt <kevinvittor579@gmail.com>
2025-09-23 00:55:12 -0300

    implementar luego

--- src/main/java/com/acelerador/polo_it_acelerador/models/dto/request/UsuarioRequestDTO.java
+++ src/main/java/com/acelerador/polo_it_acelerador/models/dto/request/UsuarioRequestDTO.java
@@ -1,5 +1,5 @@
 package com.acelerador.polo_it_acelerador.models.dto.request;
-
+// Pronta implementacion
 public record UsuarioRequestDTO() {
 
 }
--- src/main/java/com/acelerador/polo_it_acelerador/models/dto/response/UsuarioResponseDTO.java
+++ src/main/java/com/acelerador/polo_it_acelerador/models/dto/response/UsuarioResponseDTO.java
@@ -1,5 +1,5 @@
 package com.acelerador.polo_it_acelerador.models.dto.response;
-
+// Pronta implementacion
 public class UsuarioResponseDTO {
 
 }
